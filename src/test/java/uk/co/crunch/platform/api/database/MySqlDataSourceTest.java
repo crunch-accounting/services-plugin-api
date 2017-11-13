c71252c301d81df2747c62c4d2dc1dfd403ccd99
Andrew Regan <aregan@gmail.com>
2017-11-13 15:44:16 +0000

    Fix test

--- src/test/java/uk/co/crunch/platform/api/database/MySqlDataSourceTest.java
+++ src/test/java/uk/co/crunch/platform/api/database/MySqlDataSourceTest.java
@@ -18,7 +18,7 @@ public class MySqlDataSourceTest {
 
         assertThat( getConfigName(ann) ).isEqualTo("wordpress");
         assertThat( getRoleName(ann) ).isEqualTo("insurancedb_default");
-        assertThat( getDbConnectionUrl(ann) ).isEqualTo("root:mysql@tcp(rds-wordpress.x.y:3306)/");
+        assertThat( getDbConnectionUrl(ann) ).isEqualTo("@tcp(rds-wordpress.x.y:3306)/");
         assertThat( getUserCreationStatement(ann) ).isEqualTo("CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';GRANT ALL ON insurancedb.* TO '{{name}}'@'%';");
     }
 
@@ -32,7 +32,7 @@ public class MySqlDataSourceTest {
 
         assertThat( getConfigName(ann) ).isEqualTo("rds_service_consul");
         assertThat( getRoleName(ann) ).isEqualTo("insurancedb_readonly");
-        assertThat( getDbConnectionUrl(ann) ).isEqualTo("root:mysql@tcp(rds.service.consul:3306)/");
+        assertThat( getDbConnectionUrl(ann) ).isEqualTo("@tcp(rds.service.consul:3306)/");
         assertThat( getUserCreationStatement(ann) ).isEqualTo("CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';GRANT SELECT ON insurancedb.* TO '{{name}}'@'%';");
     }
 
@@ -46,7 +46,7 @@ public class MySqlDataSourceTest {
 
         assertThat( getConfigName(ann) ).isEqualTo("rds_service_consul");
         assertThat( getRoleName(ann) ).isEqualTo("blahDb_default");
-        assertThat( getDbConnectionUrl(ann) ).isEqualTo("root:mysql@tcp(rds.service.consul:3306)/");
+        assertThat( getDbConnectionUrl(ann) ).isEqualTo("@tcp(rds.service.consul:3306)/");
         assertThat( getUserCreationStatement(ann) ).isEqualTo("CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';GRANT ALL ON blahDb.* TO '{{name}}'@'%';");
     }
 }
