f36d1d8088ec30c605e2b9d30b1449519e189f2f
Andrew Regan <aregan@gmail.com>
2017-11-13 15:44:45 +0000

    Remove redundant @RabbitExchange; bump version

--- src/test/java/uk/co/crunch/platform/api/rabbit/RabbitApiTest.java
+++ src/test/java/uk/co/crunch/platform/api/rabbit/RabbitApiTest.java
@@ -1,9 +1,6 @@
 package uk.co.crunch.platform.api.rabbit;
 
 import org.junit.Test;
-import uk.co.crunch.platform.api.rabbit.RabbitExchange.Command;
-import uk.co.crunch.platform.api.rabbit.RabbitExchange.Event;
-import uk.co.crunch.platform.api.rabbit.RabbitExchange.Queue;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.co.crunch.platform.api.rabbit.RabbitCommands.*;
@@ -38,19 +35,5 @@ public class RabbitApiTest {
         assertThat( getRoleName(ann, "yearend") ).isEqualTo("special");
         assertThat( getConnectionUrl(ann) ).isEqualTo("http://rabbitmq.service.consul:15672");
         assertThat( getUserCreationStatement(ann) ).isEqualTo("{\"/\":{\"read\":\"consumer\\\\.lead\\\\.producer\\\\.lead\\\\.create\",\"configure\":\".*\",\"write\":\"default|email.*\"}}");
-    }
-
-    @RabbitExchange(name="gov-gateway", /* Default: type="direct", */ events={
-            @Event(name="gov-gateway-status-update",
-                    routingKey="event.gov-gateway.status.change",
-                    queue=@Queue(name="csmr.year-end.producer.gov-gateway.status.update", version="v1"))
-    }, commands={
-            @Command(name="gov-gateway-internal-delayed",
-                    routingKey="command.gateway.internal.poll")
-    })
-    @Test
-    public void testComprehensiveExchange() throws NoSuchMethodException {
-        final RabbitExchange ann = this.getClass().getMethod("testComprehensiveExchange").getAnnotation(RabbitExchange.class);
-        assertThat(ann).isNotNull();
     }
 }
