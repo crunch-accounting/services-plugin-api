6d72c007cc1e126c236b01509f5c28ac8d2f8622
Andrew Regan <aregan@gmail.com>
2019-12-30 15:58:24 +0000

    New CrunchTestValidationOverrides

--- src/main/java/uk/co/crunch/platform/api/tests/CrunchTestValidationOverrides.java
+++ src/main/java/uk/co/crunch/platform/api/tests/CrunchTestValidationOverrides.java
@@ -2,7 +2,9 @@ package uk.co.crunch.platform.api.tests;
 
 public enum CrunchTestValidationOverrides {
     HAMCREST_USAGE,
+    JUNIT3_ASSERTIONS,
     JUNIT4_ASSERTIONS,
+    JUNIT5_ASSERTIONS,
     KOTLIN_STRIKT_MIGRATION,
     MIXED_JUNIT4_JUNIT5
 }
