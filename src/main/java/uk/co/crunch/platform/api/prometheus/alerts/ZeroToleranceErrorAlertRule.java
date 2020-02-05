c0635c90fadcc2f4965289c50398d3d333dc3e71
Andrew Regan <aregan@gmail.com>
2020-02-05 17:01:11 +0000

    Make @ZeroToleranceErrorAlertRule repeatable

--- src/main/java/uk/co/crunch/platform/api/prometheus/alerts/ZeroToleranceErrorAlertRule.java
+++ src/main/java/uk/co/crunch/platform/api/prometheus/alerts/ZeroToleranceErrorAlertRule.java
@@ -1,10 +1,12 @@
 package uk.co.crunch.platform.api.prometheus.alerts;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Repeatable(ZeroToleranceErrorAlertRules.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
 public @interface ZeroToleranceErrorAlertRule {
--- src/test/java/uk/co/crunch/platform/api/prometheus/alerts/ZeroToleranceErrorAlertRuleUnitTest.java
+++ src/test/java/uk/co/crunch/platform/api/prometheus/alerts/ZeroToleranceErrorAlertRuleUnitTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.co.crunch.platform.api.prometheus.alerts.AlertSeverity.PAGER;
+import static uk.co.crunch.platform.api.prometheus.alerts.AlertSeverity.WARNING;
 
 public class ZeroToleranceErrorAlertRuleUnitTest {
 
@@ -21,5 +22,28 @@ public class ZeroToleranceErrorAlertRuleUnitTest {
         assertThat(ann.description()).isEqualTo("desc");
         assertThat(ann.duration()).isEqualTo("1m");
         assertThat(ann.timeRange()).isEqualTo("1m");
+    }
+
+    @ZeroToleranceErrorAlertRule(name = "mine", errorLabel = "loginErrors", team = TEAM, description = "desc")
+    @ZeroToleranceErrorAlertRule(name = "yours", errorLabel = "xErrors", team = TEAM, severity = PAGER, description = "foop")
+    @Test
+    public void testRepeatable() throws NoSuchMethodException {
+        final ZeroToleranceErrorAlertRule[] rules = this.getClass().getMethod("testRepeatable").getAnnotationsByType(ZeroToleranceErrorAlertRule.class);
+
+        assertThat(rules[0].name()).isEqualTo("mine");
+        assertThat(rules[0].errorLabel()).isEqualTo("loginErrors");
+        assertThat(rules[0].severity()).isEqualTo(WARNING);
+        assertThat(rules[0].team()).isEqualTo(TEAM);
+        assertThat(rules[0].description()).isEqualTo("desc");
+        assertThat(rules[0].duration()).isEqualTo("1m");
+        assertThat(rules[0].timeRange()).isEqualTo("1m");
+
+        assertThat(rules[1].name()).isEqualTo("yours");
+        assertThat(rules[1].errorLabel()).isEqualTo("xErrors");
+        assertThat(rules[1].severity()).isEqualTo(PAGER);
+        assertThat(rules[1].team()).isEqualTo(TEAM);
+        assertThat(rules[1].description()).isEqualTo("foop");
+        assertThat(rules[1].duration()).isEqualTo("1m");
+        assertThat(rules[1].timeRange()).isEqualTo("1m");
     }
 }
